85ae2b1572e41bf834f66d4405f1af7c0b8ff741
hannahschwen <hannahschwen@gmail.com>
2024-06-21 21:08:22 -0400

    Added files for 30 days JavaScript

--- Algorithms/10/01-linear-search/Unsolved/linear-search.js
+++ Algorithms/10/01-linear-search/Unsolved/linear-search.js
@@ -3,4 +3,10 @@
 // else return -1
 // You may not use the `indexOf`, `lastIndexOf` or `includes` methods
 
-var linearSearch = function(arr, target) {};
+var linearSearch = function(arr, target) {
+  console.log(arr);
+  console.log(target);
+  // iterate through the array, looking for target
+  // if target is included, get it's index and return it
+  // if target is not included, return -1
+};
